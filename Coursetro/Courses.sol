a38fc36466aa256f421dbe2f7fa2ba6f31da4243
vboxorcale <vboxorcale@gmail.com>
2019-08-19 17:08:00 +0800

    access wallet code changed

--- Coursetro/Courses.sol
+++ Coursetro/Courses.sol
@@ -1,28 +1,49 @@
 pragma solidity ^0.4.18;
 
-contract Courses{
+contract Owned{
+    address owner;
+    
+    function Owned()public{
+        owner= msg.sender;
+    }
+    modifier onlyOwner{
+        require(msg.sender== owner);
+        _;
+    }
+}
+
+
+contract Courses is Owned{
     struct Instructor{
         uint age;
-        string fName;
-        string lName;
+        bytes16 fName;
+        bytes16 lName;
     }
     mapping(address => Instructor) instructors;
     address[] public instructorAccts;
     
-  function setInstructor(address _address, uint _age, string _fName, string _lName)public{
+    event instructorInfo(
+        bytes16 fName,
+        bytes16 lName,
+        uint age
+        
+        );
+    
+    function setInstructor(address _address, uint _age, bytes16 _fName, bytes16 _lName)onlyOwner public{
         var instructor = instructors[_address];
         instructor.age = _age;
         instructor.fName =_fName;
         instructor.lName =_lName;
         
         instructorAccts.push(_address)-1;
+        instructorInfo(_fName, _lName,_age);
     }
     
     function getInstructors ()view public returns (address[]){
         return instructorAccts;
     }
     
-    function getInstructor(address ins)view public returns (uint, string, string){
+    function getInstructor(address ins)view public returns (uint, bytes16, bytes16){
         return (instructors[ins].age, instructors[ins].fName, instructors[ins].lName);
     }
     
